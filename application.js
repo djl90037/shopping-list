2874121836a0fadef4585071233c824827d8ebfd
djl90037 <102710633+djl90037@users.noreply.github.com>
2022-06-01 18:23:26 -0500

    Update application.js

--- application.js
+++ application.js
@@ -34,8 +34,12 @@ let updateQuantity = function () {
 let addItem = function () {
   let newItem = $('#product').val();
   let newPrice = $('#price').val();
-  $('#addItem').before('<tr class="item"><td class="name">' + newItem + '</td><td class="price">' + newPrice + '</td><td class="quantity"><input type="number" min="0" value="1"></td><td class="total"></td><td><button class="btn btn-light btn-sm remove">Remove</button></td></tr>')
-  $('tr').find('#product, #price').val('');
+  if (newItem !== (typeof String) || isNaN(newPrice)) {
+    alert('Please ensure your item and price are valid.');
+  } else {
+    $('#addItem').before('<tr class="item"><td class="name">' + newItem + '</td><td class="price">' + newPrice + '</td><td class="quantity"><input type="number" min="0" value="1"></td><td class="total"></td><td><button class="btn btn-light btn-sm remove">Remove</button></td></tr>')
+    $('tr').find('#product, #price').val('');
+  }
   updatedTotalPrice()
 }
 
